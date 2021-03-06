a89c4b1ee847ca1b08c81e3e147235211da6a8c5
hasan <hasan1993mur@gmail.com>
2021-03-06 12:46:13 +0500

    правильная версия

--- src/main/java/Dz22.java
+++ src/main/java/Dz22.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 public class Dz22 {
     public static void main(String[] args) {
         final Random random=new Random();
-        int k=0;
+        int k = 0;
         double q;
         int q2;
-        Scanner scanner=new Scanner(System.in);
-        int n=scanner.nextInt();
-        for (int i=0;i<n;i++){
-            try {
-                k=random.nextInt();
-                if (k<0) throw new Exception();
-            } catch (Exception e) {
 
-            }
-            q=Math.sqrt(k);
-            q2=(int)q;
-            if (q2*q2==k){
-                System.out.println((int)q);
-            }
+        try {
+            k=random.nextInt();
+            System.out.println(k);
+            if (k<0)throw new Exception();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+        q=Math.sqrt(k);
+        System.out.println(q);
+        q2=(int)q;
+        if (q2*q2==k){
+            System.out.println(q);
         }
+
+    }
     }
 
