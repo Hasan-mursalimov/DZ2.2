517cf8842b641cce69f9cd571839386db66c417f
hasan <hasan1993mur@gmail.com>
2021-03-09 16:10:14 +0500

    изменено

--- src/main/java/Dz22.java
+++ src/main/java/Dz22.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 
 public class Dz22 {
     public static void main(String[] args) {
-        final Random random=new Random();
+        final Random random = new Random();
         int k = 0;
         double q;
         int q2;
-
-        try {
-            k=random.nextInt();
-            System.out.println(k);
-            if (k<0)throw new Exception();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            try {
+                k = random.nextInt();
+                if (k < 0) throw new ArithmeticException();
+            } catch (ArithmeticException e) {
+                System.out.println("Число не может быть ниже нуля: " + e.getMessage());
+            }
+            q = Math.sqrt(k);
+            q2 = (int) q;
+            if (q2 * q2 == k) {
+                System.out.println((int) q);
+            }
         }
-        q=Math.sqrt(k);
-        System.out.println(q);
-        q2=(int)q;
-        if (q2*q2==k){
-            System.out.println(q);
-        }
-
     }
-    }
+}
 
